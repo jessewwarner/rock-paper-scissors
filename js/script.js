11701566bb82a40d9f57a044a409a10a0fa8343a
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-16 09:34:51 -0400

    Add game loop to play 5 rounds

--- js/script.js
+++ js/script.js
@@ -4,26 +4,71 @@ function getComputerChoice(arr){
 }
 
 function playRound(playerSelection, computerSelection){
-    console.log(`Player: ${playerSelection} Comp: ${computerSelection}`);
     if (playerSelection === computerSelection){
-        return TIE;
+        return 't';
     }
     switch(playerSelection){
         case 'rock':
-            return computerSelection === 'scissors' ? PLAYER_WINS : COMP_WINS;
-        case 'scissors':
-            return computerSelection === 'paper' ? PLAYER_WINS : COMP_WINS;
+            return computerSelection === 'scissors' ? 'p' : 'c';
         case 'paper':
-            return computerSelection === 'rock' ? PLAYER_WINS : COMP_WINS;
+            return computerSelection === 'rock' ? 'p' : 'c';
         default:
-            return 'Oh no! Something went wrong!';
+            return computerSelection === 'paper' ? 'p' : 'c';
     }
+}
+
+function game(){
+    const CANCEL_MSG = 'Player canceled the prompt!';
+    let playerWins = 0;
+    let compWins = 0;
+    let ties = 0;
+    for (let i = 0; i < 5; i++){
+        let playerChoice = prompt("Enter rock, paper, or scissors: ");
+
+        if (playerChoice === null){
+            return CANCEL_MSG;
+        }
+
+        playerChoice = playerChoice.toLowerCase()
+
+        if (!OPTIONS.includes(playerChoice)){
+            alert('Invalid choose! Please try again.');
+            i--;
+            continue;
+        }
+
+        let compChoice = getComputerChoice(OPTIONS);
+        let roundWinner = playRound(playerChoice, compChoice)
+
+        switch (roundWinner){
+            case 'p':
+                playerWins++;
+                break;
+            case 'c':
+                compWins++;
+                break;
+            default:
+                ties++;
+                break;
+        }
+    }
+
+    console.log(`Player: ${playerWins} Computer: ${compWins} Ties: ${ties}`);
+    
+    let winner;
+    if (playerWins > compWins){
+        winner = PLAYER_WINS;
+    } else if (compWins > playerWins){
+        winner = COMP_WINS;
+    } else {
+        winner = TIE;
+    }
+    return winner;
 }
 
 const OPTIONS = ['rock', 'paper', 'scissors'];
 const PLAYER_WINS = 'Congratulations! You win!'
-const COMP_WINS = 'Sorry!, Computer Wins!'
+const COMP_WINS = 'Sorry! Computer Wins!'
 const TIE = "It's a tie!"
 
-let winner = playRound('rock', getComputerChoice(OPTIONS));
-console.log(winner);
+console.log(game());
