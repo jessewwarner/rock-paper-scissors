5edebebf393f50d173a13a2d07b6adf6f5a12513
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-16 08:48:29 -0400

    Add function to play a round

--- js/script.js
+++ js/script.js
@@ -3,5 +3,27 @@ function getComputerChoice(arr){
     return arr[index];
 }
 
-const OPTIONS = ['Rock', 'Paper', 'Scissors'];
-console.log(getComputerChoice(OPTIONS));
+function playRound(playerSelection, computerSelection){
+    console.log(`Player: ${playerSelection} Comp: ${computerSelection}`);
+    if (playerSelection === computerSelection){
+        return TIE;
+    }
+    switch(playerSelection){
+        case 'rock':
+            return computerSelection === 'scissors' ? PLAYER_WINS : COMP_WINS;
+        case 'scissors':
+            return computerSelection === 'paper' ? PLAYER_WINS : COMP_WINS;
+        case 'paper':
+            return computerSelection === 'rock' ? PLAYER_WINS : COMP_WINS;
+        default:
+            return 'Oh no! Something went wrong!';
+    }
+}
+
+const OPTIONS = ['rock', 'paper', 'scissors'];
+const PLAYER_WINS = 'Congratulations! You win!'
+const COMP_WINS = 'Sorry!, Computer Wins!'
+const TIE = "It's a tie!"
+
+let winner = playRound('rock', getComputerChoice(OPTIONS));
+console.log(winner);
